01509c2888801c765eaf30abaf69c00fb3c3e2ad
liuxueyang <liuxueyang457@gmail.com>
2024-06-11 23:53:46 +0800

    [WIP] daimayuan 659

---
+++ daimayuan/659.cpp
@@ -0,0 +1,288 @@
+// Date: Tue Jun 11 23:07:10 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998244353;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T> void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &i : x)
+    cerr << (f++ ? ", " : ""), __print(i);
+  cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x " = " << x << endl;
+#define debug2(x, y) cout << #x " = " << x << " " #y " = " << y << endl;
+#define debug3(x, y, z)                                                        \
+  cout << #x " = " << x << " " #y " = " << y << " " #z " = " << z << endl;
+#define dbg(x...)                                                              \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = [";    \
+  _print(x);                                                                   \
+  cerr << "\e[39m" << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#define dbg(x...)
+#endif
+
+const int N = 200100;
+int n, q, a[N];
+
+struct tag {
+  ll val;
+  tag() : val(0) {}
+  tag(int x) : val(x) {}
+};
+
+struct info {
+  ll val;
+  info() {}
+  info(int x) : val(x) {}
+};
+
+tag operator+(const tag &l, const tag &r) { return tag(l.val + r.val); }
+
+info operator+(const info &l, const tag &t) { return info(l.val + t.val); }
+
+info operator+(const info &l, const info &r) { return info(max(l.val, r.val)); }
+
+struct SegmentTree {
+  int n;
+  struct node {
+    info val;
+    tag t;
+  };
+
+  vector<node> seg;
+
+  SegmentTree(int n_) : n(n_) { seg = vector<node>(n * 4 + 10); }
+
+  void set_tag(int id, tag t) {
+    seg[id].val = seg[id].val + t;
+    seg[id].t = seg[id].t + t;
+  }
+
+  void push_down(int id) {
+    tag t = seg[id].t;
+    if (t.val != 0) {
+      seg[id].t = tag(0);
+
+      int left = id * 2, right = left + 1;
+      set_tag(left, t);
+      set_tag(right, t);
+    }
+  }
+
+  void update(int id) {
+    int left = id * 2, right = left + 1;
+    seg[id].val = seg[left].val + seg[right].val;
+  }
+
+  void build(int id, int l, int r) {
+    if (l == r) {
+      seg[id].val = info(a[l]);
+      return;
+    }
+
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    build(left, l, mid);
+    build(right, mid + 1, r);
+    update(id);
+  }
+
+  void change(int id, int l, int r, int pos, int x) {
+    if (l == r) {
+      seg[id].val = info(x);
+      return;
+    }
+
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    if (pos <= mid)
+      change(left, l, mid, pos, x);
+    else
+      change(right, mid + 1, r, pos, x);
+
+    update(id);
+  }
+
+  info query(int id, int l, int r, int ql, int qr) {
+    if (ql == l && qr == r) {
+      return seg[id].val;
+    }
+
+    push_down(id);
+
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+
+    if (qr <= mid)
+      return query(left, l, mid, ql, qr);
+    if (ql >= mid + 1)
+      return query(right, mid + 1, r, ql, qr);
+
+    return query(left, l, mid, ql, mid) + query(right, mid + 1, r, mid + 1, qr);
+  }
+
+  void modify(int id, int l, int r, int ql, int qr, tag t) {
+    if (ql == l && qr == r) {
+      set_tag(id, t);
+      return;
+    }
+
+    push_down(id);
+
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    if (qr <= mid)
+      modify(left, l, mid, ql, qr, t);
+    else if (ql >= mid + 1)
+      modify(right, mid + 1, r, ql, qr, t);
+    else {
+      modify(left, l, mid, ql, mid, t);
+      modify(right, mid + 1, r, mid + 1, qr, t);
+    }
+
+    update(id);
+  }
+};
+
+SegmentTree tr(10);
+
+void solve() {
+  cin >> n >> q;
+
+  For1(i, 1, n) { cin >> a[i]; }
+
+  tr = SegmentTree(n);
+  tr.build(1, 1, n);
+
+  while (q--) {
+    int op;
+    cin >> op;
+    if (op == 1) {
+      int l, r, d;
+      cin >> l >> r >> d;
+      tr.modify(1, 1, n, l, r, tag(d));
+    } else {
+      int l, r;
+      cin >> l >> r;
+      info res = tr.query(1, 1, n, l, r);
+      cout << res.val << '\n';
+    }
+  }
+}
+
+int main(void) {
+#ifdef _DEBUG
+  freopen("659.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int T = 1;
+
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
