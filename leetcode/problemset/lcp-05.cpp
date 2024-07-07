cf716502ee46843e477833a1e9fd8a75b940b954
liuxueyang <liuxueyang457@gmail.com>
2024-07-07 20:27:29 +0800

    Finish LeetCode LCP-05

--- leetcode/problemset/lcp-05.cpp
+++ leetcode/problemset/lcp-05.cpp
@@ -155,22 +155,144 @@ struct TreeNode {
 const int N = 50100;
 
 int n, idx, h[N];
-int e[N], ne[N];
+int e[N], ne[N], sz[N], pos, a[N];
 
 void Init() {
   memset(h, -1, sizeof h);
+  memset(sz, 0, sizeof sz);
   idx = 0;
+  pos = 0;
 }
 
 void add(int a, int b) { e[idx] = b, ne[idx] = h[a], h[a] = idx++; }
 
-struct info {};
+int dfs(int i) {
+  a[++pos] = i;
+  sz[i] = 1;
 
-struct tag {};
+  for (int j = h[i]; j != -1; j = ne[j]) {
+    int k = e[j];
+    sz[i] += dfs(k);
+  }
+
+  return sz[i];
+}
+
+struct info {
+  int sum, len;
+  info() : sum(0), len(0) {}
+  info(int _s, int _l) : sum(_s % MOD), len(_l) {}
+};
+
+struct tag {
+  int d;
+  tag() : d(0) {}
+  tag(int _d) : d(_d) {}
+};
+
+tag operator+(const tag &l, const tag &r) { return tag(l.d + r.d); }
+
+info operator+(const info &i, const tag &t) {
+  return info(i.sum + ll(t.d) * i.len % MOD, i.len);
+}
+
+info operator+(const info &l, const info &r) {
+  return info(l.sum + r.sum, l.len + r.len);
+}
 
 struct SegmentTree {
-  struct node {};
+  struct node {
+    info val;
+    tag t;
+  };
+
   int n;
+  VI a;
+  vector<node> seg;
+
+  SegmentTree(int _n, VI &_a) : n(_n), a(_a) {
+    a.insert(a.begin(), 0);
+    seg = vector<node>(n * 4);
+  }
+
+  void set_tag(int id, tag t) {
+    seg[id].val = seg[id].val + t;
+    seg[id].t = seg[id].t + t;
+  }
+
+  void push_down(int id) {
+    int left = id * 2, right = left + 1;
+    tag &t1 = seg[id].t;
+
+    if (t1.d) {
+      set_tag(left, t1);
+      set_tag(right, t1);
+      t1 = tag();
+    }
+  }
+
+  void update(int id) {
+    int left = id * 2, right = left + 1;
+    seg[id].val = seg[left].val + seg[right].val;
+  }
+
+  void build(int id, int l, int r) {
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    if (l == r) {
+      seg[id].val = info(a[l], 1);
+      return;
+    }
+    build(left, l, mid);
+    build(right, mid + 1, r);
+    update(id);
+  }
+
+  void change(int id, int l, int r, int pos, int d) {
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    if (l == r) {
+      seg[id].val.sum += d;
+      return;
+    }
+    push_down(id);
+    if (pos <= mid)
+      change(left, l, mid, pos, d);
+    else
+      change(right, mid + 1, r, pos, d);
+    update(id);
+  }
+
+  void modify(int id, int l, int r, int ql, int qr, tag t) {
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    if (ql == l && qr == r) {
+      set_tag(id, t);
+      return;
+    }
+    push_down(id);
+    if (qr <= mid)
+      modify(left, l, mid, ql, qr, t);
+    else if (ql > mid)
+      modify(right, mid + 1, r, ql, qr, t);
+    else {
+      modify(left, l, mid, ql, mid, t);
+      modify(right, mid + 1, r, mid + 1, qr, t);
+    }
+    update(id);
+  }
+
+  info query(int id, int l, int r, int ql, int qr) {
+    int left = id * 2, right = left + 1, mid = (l + r) / 2;
+    if (l == ql && r == qr) {
+      return seg[id].val;
+    }
+    push_down(id);
+    if (qr <= mid)
+      return query(left, l, mid, ql, qr);
+    else if (ql > mid)
+      return query(right, mid + 1, r, ql, qr);
+    else
+      return query(left, l, mid, ql, mid) +
+             query(right, mid + 1, r, mid + 1, qr);
+  }
 };
 
 class Solution {
@@ -182,6 +304,32 @@ public:
       int x = v[0], y = v[1];
       add(x, y);
     }
+    dfs(1);
+
+    VI b(n, 0);
+
+    SegmentTree tr(n, b);
+    tr.build(1, 1, n);
+
+    map<int, int> m;
+    VI ans;
+
+    For1(i, 1, n) { m[a[i]] = i; }
+
+    for (auto &v : que) {
+      if (v[0] == 1) {
+        tr.change(1, 1, n, m[v[1]], v[2]);
+      } else if (v[0] == 2) {
+        int s1 = m[v[1]], s2 = s1 + sz[v[1]] - 1;
+        tr.modify(1, 1, n, s1, s2, v[2]);
+      } else {
+        int s1 = m[v[1]], s2 = s1 + sz[v[1]] - 1;
+        info i1 = tr.query(1, 1, n, s1, s2);
+        ans.pb(i1.sum);
+      }
+    }
+
+    return ans;
   }
 };
 
@@ -193,6 +341,13 @@ int main(void) {
   cout.tie(NULL);
 
   Solution a;
+  vector<VI> p1, p2;
+  VI res;
+
+  p1 = {{1, 2}, {1, 6}, {2, 3}, {2, 5}, {1, 4}};
+  p2 = {{1, 1, 500}, {2, 2, 50}, {3, 1}, {2, 6, 15}, {3, 1}};
+  res = a.bonus(6, p1, p2);
+  dbg(res);
 
   return 0;
 }
