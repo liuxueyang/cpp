7723002e221d82a4bafc69e3d5494d6db6e29dc7
liuxueyang <liuxueyang457@gmail.com>
2024-08-05 00:44:23 +0800

    CodeForces contest 963 div2 AB [WIP C]

---
+++ codeforces/contest/963_div2/b.cpp
@@ -0,0 +1,224 @@
+// Date: Sun Aug  4 22:46:47 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998244353;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+template <typename ForwardIterator>
+void Inputr(ForwardIterator begin, ForwardIterator end) {
+  ForwardIterator it = begin;
+  while (it != end) {
+    cin >> *it;
+    it++;
+  }
+}
+
+template <typename ForwardIterator>
+void Outputr(ForwardIterator begin, ForwardIterator end) {
+  ForwardIterator it = begin;
+  while (it != end) {
+    if (it != begin)
+      cout << ' ';
+    cout << *it;
+    it++;
+  }
+  NL;
+}
+
+template <typename T, typename ForwardIterator>
+void Outputr1(ForwardIterator begin, ForwardIterator end) {
+  copy(begin, end, ostream_iterator<T>(cout, " "));
+  NL;
+}
+
+// int128 input and output
+#ifdef _DEBUG
+using lll = __int128;
+
+istream &operator>>(istream &is, lll &v) {
+  string s;
+  is >> s;
+  v = 0;
+  for (auto &it : s)
+    if (isdigit(it))
+      v = v * 10 + it - '0';
+  if (s[0] == '-')
+    v *= -1;
+  return is;
+}
+
+ostream &operator<<(ostream &os, const lll &v) {
+  if (v == 0)
+    return (os << "0");
+  lll num = v;
+  if (v < 0)
+    os << '-', num = -num;
+  string s;
+  for (; num > 0; num /= 10)
+    s.pb((char)(num % 10) + '0');
+  reverse(all(s));
+  return (os << s);
+}
+#endif
+// end of int128
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+void solve() {
+  int n;
+  cin >> n;
+
+  VI a(n);
+  Inputr(all(a));
+
+  int cnt0{}, cnt1{};
+  ll mx1 = -INF;
+  VI v0;
+
+  for (auto x : a) {
+    if (x & 1) {
+      cnt1++;
+      ckmax(mx1, ll(x));
+    } else {
+      cnt0++;
+      v0.pb(x);
+    }
+  }
+  if (cnt0 == 0 || cnt1 == 0) {
+    cout << "0\n";
+    return;
+  }
+
+  ll ans{};
+  ll mx2 = mx1;
+  sort(all(v0), greater<int>());
+
+  ll ans1{};
+  for (auto x : v0) {
+    if (x < mx1) {
+      ans++;
+      x = x + mx1;
+      mx1 = x;
+    } else {
+      ans++;
+      mx1 += x;
+      ans++;
+      mx1 += x;
+    }
+  }
+
+  mx1 = mx2;
+  sort(all(v0));
+  for (auto x : v0) {
+    if (x < mx1) {
+      ans1++;
+      mx1 += x;
+    } else {
+      dbg(mx1, x, ans1);
+      ans1 += 2;
+      mx1 += x;
+      mx1 += x;
+    }
+  }
+
+  dbg(ans, ans1, mx1, mx2, v0);
+  ckmin(ans, ans1);
+  cout << ans << '\n';
+}
+
+int main(void) {
+#ifdef _DEBUG
+  freopen("b.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int T = 1;
+  cin >> T;
+
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
