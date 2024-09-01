7df5304bf5411dd4fe9b23c94fa28d8a63cd8c0f
liuxueyang <liuxueyang457@gmail.com>
2024-09-01 15:39:15 +0800

    LeetCode 2096

---
+++ leetcode/problemset/2096.cpp
@@ -0,0 +1,208 @@
+// Date: Sun Sep  1 14:27:33 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+#endif
+// End of LeetCode
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  static const int N = 100100;
+  char s[N];
+  string ans;
+  bool ok{};
+  int dist{};
+  string path{};
+
+  void dfs2(TNP root, int ta, int idx) {
+    if (!root || ok)
+      return;
+
+    auto [val, l, r] = *root;
+    if (val == ta) {
+      ok = true;
+      dist = idx - 1;
+      path = string(s, s + idx - 1);
+      return;
+    }
+
+    s[idx - 1] = 'L';
+    dfs2(l, ta, idx + 1);
+    s[idx - 1] = 'R';
+    dfs2(r, ta, idx + 1);
+  }
+
+  int get_dist(TNP root, int ta) {
+    ok = false;
+    dist = 0;
+    path = {};
+    dfs2(root, ta, 1);
+
+    return dist;
+  }
+
+  TNP dfs(TNP root, int st, int en) {
+    if (!root || root->val == st || root->val == en)
+      return root;
+
+    auto [val, l, r] = *root;
+    auto lcal = dfs(l, st, en), lcar = dfs(r, st, en);
+    if (lcal && lcar) {
+      return root;
+    }
+    return lcal ? lcal : lcar;
+  }
+
+  string getDirections(TreeNode *root, int startValue, int destValue) {
+    auto lca = dfs(root, startValue, destValue);
+    int val = lca->val;
+
+    if (val == startValue) {
+      get_dist(lca, destValue);
+      return path;
+    } else if (val == destValue) {
+      int d = get_dist(lca, startValue);
+      return string(d, 'U');
+    } else {
+      int d = get_dist(lca, startValue);
+      ans += string(d, 'U');
+      get_dist(lca, destValue);
+      ans += path;
+      return ans;
+    }
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  Solution a;
+
+  return 0;
+}
+
+#endif
