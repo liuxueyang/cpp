cdf77ea4ad6eba96ceaae234424a9b2c62b221d1
liuxueyang <liuxueyang457@gmail.com>
2024-09-05 15:41:28 +0800

    LeetCode lcp-34

---
+++ leetcode/problemset/lcp-34.cpp
@@ -0,0 +1,211 @@
+// Date: Thu Sep  5 13:47:58 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+#endif
+
+#define null NULL
+
+TNP BuildLCTree(VI a) {
+  TNP root{};
+  int n = SZ(a);
+  if (!n)
+    return root;
+  vector<TNP> b(n + 1, nullptr);
+
+  For(i, 0, n) {
+    int j = i + 1;
+    TNP cur{new TN(a[i])};
+    if (j == 1) {
+      root = cur;
+    } else {
+      int p = j / 2;
+      TNP pp = b[p];
+      if (j & 1)
+        pp->right = cur;
+      else
+        pp->left = cur;
+    }
+    b[j] = cur;
+  }
+  return root;
+}
+
+void PrePrintLCTree(TNP root) {
+  if (!root)
+    return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+// End of LeetCode
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  int k;
+
+  VI dfs(TNP root) {
+    if (!root) {
+      VI res(k + 1, -INF);
+      res[0] = 0;
+      return res;
+    }
+
+    auto [val, l, r] = *root;
+    auto v1 = dfs(l), v2 = dfs(r);
+
+    VI res(k + 1);
+
+    res[0] = *max_element(all(v1)) + *max_element(all(v2));
+
+    For1(i, 1, k) {
+      For1(j, 0, i - 1) { ckmax(res[i], v1[j] + v2[i - 1 - j] + val); }
+    }
+
+    return res;
+  }
+
+  int maxValue(TreeNode *root, int k_) {
+    k = k_;
+    auto res = dfs(root);
+    return *max_element(all(res));
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  Solution a;
+  VI ve{4, 1, 3, 9, null, null, 2};
+  int k = 2;
+
+  TNP tree = BuildLCTree(ve);
+  int res = a.maxValue(tree, k);
+  dbg(res);
+
+  return 0;
+}
+
+#endif
