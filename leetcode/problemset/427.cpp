065b7f5194cafa1376cf59d0a03842674460a480
liuxueyang <liuxueyang457@gmail.com>
2024-09-07 22:09:41 +0800

    LeetCode 427

---
+++ leetcode/problemset/427.cpp
@@ -0,0 +1,292 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T>
+bool ckmin(T &a, const T &b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T &a, const T &b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <typename t>
+istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec) in >> x;
+  return in;
+}
+
+template <typename t>
+ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1) out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+class Node {
+ public:
+  bool val;
+  bool isLeaf;
+  Node *topLeft;
+  Node *topRight;
+  Node *bottomLeft;
+  Node *bottomRight;
+
+  Node() {
+    val = false;
+    isLeaf = false;
+    topLeft = NULL;
+    topRight = NULL;
+    bottomLeft = NULL;
+    bottomRight = NULL;
+  }
+
+  Node(bool _val, bool _isLeaf) {
+    val = _val;
+    isLeaf = _isLeaf;
+    topLeft = NULL;
+    topRight = NULL;
+    bottomLeft = NULL;
+    bottomRight = NULL;
+  }
+
+  Node(bool _val, bool _isLeaf, Node *_topLeft, Node *_topRight,
+       Node *_bottomLeft, Node *_bottomRight) {
+    val = _val;
+    isLeaf = _isLeaf;
+    topLeft = _topLeft;
+    topRight = _topRight;
+    bottomLeft = _bottomLeft;
+    bottomRight = _bottomRight;
+  }
+};
+#endif
+
+#define null NULL
+
+TNP BuildLCTree(VI a) {
+  TNP root{};
+  int n = SZ(a);
+  if (!n) return root;
+  vector<TNP> b(n + 1, nullptr);
+
+  For(i, 0, n) {
+    int j = i + 1;
+    TNP cur{new TN(a[i])};
+    if (j == 1) {
+      root = cur;
+    } else {
+      int p = j / 2;
+      TNP pp = b[p];
+      if (j & 1)
+        pp->right = cur;
+      else
+        pp->left = cur;
+    }
+    b[j] = cur;
+  }
+  return root;
+}
+
+void PrePrintLCTree(TNP root) {
+  if (!root) return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+// End of LeetCode
+
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node*
+_bottomLeft, Node* _bottomRight) { val = _val; isLeaf = _isLeaf; topLeft =
+_topLeft; topRight = _topRight; bottomLeft = _bottomLeft; bottomRight =
+_bottomRight;
+    }
+};
+*/
+
+class Solution {
+ public:
+  vector<VI> a, b;
+  int n;
+
+  Node *dfs(int x1, int x2, int y1, int y2) {
+    if (x1 == x2 && y1 == y2) {
+      return new Node(a[x1][y1], true);
+    }
+
+    int sum = b[x2][y2] - b[x1 - 1][y2] - b[x2][y1 - 1] + b[x1 - 1][y1 - 1];
+    int cnt = (x2 - x1 + 1) * (y2 - y1 + 1);
+
+    if (sum == cnt) {
+      return new Node(true, true);
+    } else if (sum == 0) {
+      return new Node(false, true);
+    }
+
+    int midx = (x1 + x2) / 2, midy = (y1 + y2) / 2;
+    return new Node(false, false, dfs(x1, midx, y1, midy),
+                    dfs(x1, midx, midy + 1, y2), dfs(midx + 1, x2, y1, midy),
+                    dfs(midx + 1, x2, midy + 1, y2));
+  }
+
+  Node *construct(vector<vector<int>> &grid) {
+    n = SZ(grid);
+    a = vector<VI>(n + 1, VI(n + 1, 0));
+
+    For(i, 0, n) {
+      For(j, 0, n) { a[i + 1][j + 1] = grid[i][j]; }
+    }
+
+    b = a;
+    For1(i, 1, n) {
+      For1(j, 1, n) { b[i][j] += b[i][j - 1]; }
+    }
+
+    For1(j, 1, n) {
+      For1(i, 1, n) { b[i][j] += b[i - 1][j]; }
+    }
+
+    return dfs(1, n, 1, n);
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  Solution a;
+  vector<VI> v{
+      {0, 1},
+      {1, 0},
+  };
+  a.construct(v);
+
+  return 0;
+}
+
+#endif
