7053bf4212c83b5c63dd4e1e7576c24a77d561d9
liuxueyang <liuxueyang457@gmail.com>
2024-10-02 12:26:06 +0800

    2 solutions for LeetCode 211

---
+++ leetcode/problemset/211_1.cpp
@@ -0,0 +1,291 @@
+// Date: Wed Oct  2 11:49:23 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+void PrePrintLCTree(TNP root) {
+  if (!root)
+    return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+
+class LCCodec {
+public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root) {
+    if (!root)
+      return "";
+
+    vector<TNP> a;
+    a.pb(root);
+    string ans;
+
+    while (nemp(a)) {
+      vector<TNP> b;
+
+      string tmp;
+      for (auto x : a) {
+        if (nemp(ans)) {
+          ans += ',';
+        }
+
+        if (x)
+          ans += to_string(x->val);
+        else
+          ans += "null";
+      }
+
+      bool ok{false};
+      for (auto x : a) {
+        if (x) {
+          b.pb(x->left);
+          b.pb(x->right);
+
+          if (x->left || x->right) {
+            ok = true;
+          }
+        }
+      }
+
+      if (ok)
+        a = std::move(b);
+      else
+        a = {};
+    }
+
+    return ans;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data) {
+    vector<TNP> a;
+    int n = SZ(data);
+    vector<string> b;
+
+    if (!n)
+      return nullptr;
+
+    string t;
+    for (auto x : data) {
+      if (x == ',') {
+        b.pb(t);
+        t = "";
+      } else
+        t += x;
+    }
+    b.pb(t);
+
+    for (auto x : b) {
+      if (x == "null")
+        a.pb(nullptr);
+      else
+        a.pb(new TN(stoi(x)));
+    }
+
+    int m = SZ(a);
+    int i = 0, j = 1;
+
+    while (i < m) {
+      while (i < m && !a[i])
+        ++i;
+      if (i >= n)
+        break;
+
+      if (j < m)
+        a[i]->left = a[j++];
+      if (j < m)
+        a[i]->right = a[j++];
+      ++i;
+    }
+
+    return a[0];
+  }
+};
+
+#endif
+// End of LeetCode
+
+class WordDictionary {
+public:
+  struct node {
+    int cnt;
+    vector<node *> son;
+
+    node() { cnt = 0, son = vector<node *>(26); }
+  };
+
+  node *root;
+
+  WordDictionary() { root = new node(); }
+
+  void addWord(string s) {
+    node *p{root};
+    for (auto c : s) {
+      int id = c - 'a';
+      if (!p->son[id])
+        p->son[id] = new node();
+      p = p->son[id];
+    }
+    p->cnt++;
+  }
+
+  bool dfs(string &s, int idx, node *p) {
+    if (idx == SZ(s))
+      return p->cnt > 0;
+
+    if (s[idx] == '.') {
+      For(i, 0, 26) {
+        auto child = p->son[i];
+        if (!child)
+          continue;
+        auto res = dfs(s, idx + 1, child);
+        if (res) {
+          return true;
+        }
+      }
+    } else {
+      int id = s[idx] - 'a';
+      auto child = p->son[id];
+      if (!child)
+        return false;
+      return dfs(s, idx + 1, child);
+    }
+    return false;
+  }
+  bool search(string s) {
+    return dfs(s, 0, root);
+  }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  // Solution a;
+
+  return 0;
+}
+
+#endif
