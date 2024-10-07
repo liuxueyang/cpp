0f2a0c3d9a7677541d1ed9ea710da218bc41474a
liuxueyang <liuxueyang457@gmail.com>
2024-10-07 13:10:33 +0800

    [WIP] Another solution for LeetCode 355

---
+++ leetcode/problemset/355_1.cpp
@@ -0,0 +1,348 @@
+// Date: Mon Oct  7 12:31:17 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+void PrePrintLCTree(TNP root) {
+  if (!root)
+    return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+
+class LCCodec {
+public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root) {
+    if (!root)
+      return "";
+
+    vector<TNP> a;
+    a.pb(root);
+    string ans;
+
+    while (nemp(a)) {
+      vector<TNP> b;
+
+      string tmp;
+      for (auto x : a) {
+        if (nemp(ans)) {
+          ans += ',';
+        }
+
+        if (x)
+          ans += to_string(x->val);
+        else
+          ans += "null";
+      }
+
+      bool ok{false};
+      for (auto x : a) {
+        if (x) {
+          b.pb(x->left);
+          b.pb(x->right);
+
+          if (x->left || x->right) {
+            ok = true;
+          }
+        }
+      }
+
+      if (ok)
+        a = std::move(b);
+      else
+        a = {};
+    }
+
+    return ans;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data) {
+    vector<TNP> a;
+    int n = SZ(data);
+    vector<string> b;
+
+    if (!n)
+      return nullptr;
+
+    string t;
+    for (auto x : data) {
+      if (x == ',') {
+        b.pb(t);
+        t = "";
+      } else
+        t += x;
+    }
+    b.pb(t);
+
+    for (auto x : b) {
+      if (x == "null")
+        a.pb(nullptr);
+      else
+        a.pb(new TN(stoi(x)));
+    }
+
+    int m = SZ(a);
+    int i = 0, j = 1;
+
+    while (i < m) {
+      while (i < m && !a[i])
+        ++i;
+      if (i >= n)
+        break;
+
+      if (j < m)
+        a[i]->left = a[j++];
+      if (j < m)
+        a[i]->right = a[j++];
+      ++i;
+    }
+
+    return a[0];
+  }
+};
+
+#endif
+// End of LeetCode
+
+class Twitter {
+public:
+  struct node {
+    int ts, id, uid;
+
+    bool operator<(const node &rh) const { return ts < rh.ts; }
+    bool operator>(const node &rh) const { return ts > rh.ts; }
+  };
+  int ts;
+  map<int, set<int>> fol;
+  map<int, set<int>> fan;
+  map<int, set<node>> twl;
+  map<int, pqg<node>> feed;
+
+  Twitter() {
+    ts = 0;
+    fol = {}, twl = {};
+  }
+
+  void postTweet(int userId, int tweetId) {
+    auto &tu = twl[userId];
+    auto tw = node{ts, tweetId, userId};
+    tu.insert(tw);
+    ts++;
+
+    if (SZ(tu) > 10) {
+      auto it = tu.begin();
+      tu.erase(it);
+    }
+
+    auto &f1 = feed[userId];
+    if (SZ(f1) < 10)
+      f1.push(tw);
+
+    auto &f2 = fan[userId];
+    for (auto u : f2) {
+      auto &s = feed[u];
+      if (SZ(s) < 10)
+        s.push(tw);
+      else {
+        s.pop();
+        s.push(tw);
+      }
+    }
+  }
+
+  vector<int> getNewsFeed(int userId) {
+    auto q = feed[userId];
+    VI ans;
+
+    while (nemp(q)) {
+      auto t = q.top();
+      q.pop();
+      ans.pb(t.id);
+    }
+    reverse(all(ans));
+    return ans;
+  }
+
+  void follow(int followerId, int followeeId) {
+    if (has(fol[followerId], followeeId))
+      return;
+
+    fol[followerId].insert(followeeId);
+    fan[followeeId].insert(followerId);
+
+    update_feed(followerId, followeeId);
+  }
+
+  void unfollow(int followerId, int followeeId) {
+    if (!has(fol[followerId], followeeId))
+      return;
+
+    fol[followerId].erase(followeeId);
+    fan[followeeId].erase(followerId);
+
+    feed[followerId] = {};
+    update_feed_uid(followerId);
+  }
+
+  void update_feed(int uid, int folid) {
+    auto &q = feed[uid];
+    auto &s = twl[folid];
+
+    for (auto it = s.rbegin(); it != s.rend(); it++) {
+      if (SZ(q) < 10)
+        q.push(*it);
+      else {
+        if (q.top().ts > it->ts)
+          break;
+        else
+          q.push(*it);
+      }
+    }
+  }
+
+  void update_feed_uid(int uid) {
+    update_feed(uid, uid);
+
+    auto &s = fol[uid];
+    for (auto y : s) {
+      update_feed(uid, y);
+    }
+  }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  // Solution a;
+
+  return 0;
+}
+
+#endif
