4b8777998ef9f36043ef49bb56d0956ffdda48d6
liuxueyang <liuxueyang457@gmail.com>
2024-10-24 23:22:40 +0800

    LeetCode 2435

---
+++ leetcode/problemset/2435.cpp
@@ -0,0 +1,301 @@
+// Date: Thu Oct 24 23:03:18 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+void PrePrintLCTree(TNP root) {
+  if (!root)
+    return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+
+class LCCodec {
+public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root) {
+    if (!root)
+      return "";
+
+    vector<TNP> a;
+    a.pb(root);
+    string ans;
+
+    while (nemp(a)) {
+      vector<TNP> b;
+
+      string tmp;
+      for (auto x : a) {
+        if (nemp(ans)) {
+          ans += ',';
+        }
+
+        if (x)
+          ans += to_string(x->val);
+        else
+          ans += "null";
+      }
+
+      bool ok{false};
+      for (auto x : a) {
+        if (x) {
+          b.pb(x->left);
+          b.pb(x->right);
+
+          if (x->left || x->right) {
+            ok = true;
+          }
+        }
+      }
+
+      if (ok)
+        a = std::move(b);
+      else
+        a = {};
+    }
+
+    return ans;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data) {
+    vector<TNP> a;
+    int n = SZ(data);
+    vector<string> b;
+
+    if (!n)
+      return nullptr;
+
+    string t;
+    for (auto x : data) {
+      if (x == ',') {
+        b.pb(t);
+        t = "";
+      } else
+        t += x;
+    }
+    b.pb(t);
+
+    for (auto x : b) {
+      if (x == "null")
+        a.pb(nullptr);
+      else
+        a.pb(new TN(stoi(x)));
+    }
+
+    int m = SZ(a);
+    int i = 0, j = 1;
+
+    while (i < m) {
+      while (i < m && !a[i])
+        ++i;
+      if (i >= n)
+        break;
+
+      if (j < m)
+        a[i]->left = a[j++];
+      if (j < m)
+        a[i]->right = a[j++];
+      ++i;
+    }
+
+    return a[0];
+  }
+};
+
+LNP CreateList(VI &a) {
+  LN *du{new LN(0)}, *cur{}, *pre{du};
+
+  for (auto x : a) {
+    cur = new LN(x);
+    pre->next = cur;
+    pre = cur;
+  }
+
+  auto res = du->next;
+  delete du;
+  return res;
+}
+
+void PrintList(LNP head) {
+  while (head) {
+    dbgi(head->val);
+    head = head->next;
+  }
+  dbgln();
+}
+
+#endif
+// End of LeetCode
+
+class Solution {
+public:
+  int numberOfPaths(vector<vector<int>> &a, int k) {
+    using VL = vector<ll>;
+    int n{SZ(a)}, m{SZ(a[0])};
+    vector<vector<VL>> d(n + 10, vector<VL>(m + 10, VL(k + 5)));
+
+    For(i, 0, n) {
+      For(j, 0, m) { a[i][j] %= k; }
+    }
+
+    d[0][0][a[0][0]] = 1;
+    int pre = a[0][0];
+    For(j, 1, m) {
+      pre = (pre + a[0][j]) % k;
+      d[0][j][pre] = 1;
+    }
+    pre = a[0][0];
+    For(i, 1, n) {
+      pre += a[i][0];
+      pre %= k;
+      d[i][0][pre] = 1;
+    }
+
+    For(i, 1, n) {
+      For(j, 1, m) {
+        For(k1, 0, k) {
+          int cnt = d[i - 1][j][k1];
+          int ne = (k1 + a[i][j]) % k;
+          auto &cur = d[i][j][ne];
+
+          if (cnt)
+            cur = (cur + cnt) % MOD;
+
+          cnt = d[i][j - 1][k1];
+          if (cnt)
+            cur = (cur + cnt) % MOD;
+        }
+      }
+    }
+
+    return d[n - 1][m - 1][0] % MOD;
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  Solution a;
+  vector<VI> ve{{5, 2, 4}, {3, 0, 5}, {0, 7, 2}};
+  auto res = a.numberOfPaths(ve, 3);
+  dbg(res);
+
+  return 0;
+}
+
+#endif
