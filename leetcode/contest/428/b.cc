3bef71aaa275090147edc80a2a7c30fb3b700975
liuxueyang <liuxueyang457@gmail.com>
2024-12-15 12:06:51 +0800

    LeetCode contest 428 AB

---
+++ leetcode/contest/428/b.cc
@@ -0,0 +1,353 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T>
+bool ckmin(T &a, const T &b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T &a, const T &b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <typename t>
+istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec) in >> x;
+  return in;
+}
+
+template <typename t>
+ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1) out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+void PrePrintLCTree(TNP root) {
+  if (!root) return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+
+class LCCodec {
+ public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root) {
+    if (!root) return "";
+
+    vector<TNP> a;
+    a.pb(root);
+    string ans;
+
+    while (nemp(a)) {
+      vector<TNP> b;
+
+      string tmp;
+      for (auto x : a) {
+        if (nemp(ans)) {
+          ans += ',';
+        }
+
+        if (x)
+          ans += to_string(x->val);
+        else
+          ans += "null";
+      }
+
+      bool ok{false};
+      for (auto x : a) {
+        if (x) {
+          b.pb(x->left);
+          b.pb(x->right);
+
+          if (x->left || x->right) {
+            ok = true;
+          }
+        }
+      }
+
+      if (ok)
+        a = std::move(b);
+      else
+        a = {};
+    }
+
+    return ans;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data) {
+    vector<TNP> a;
+    int n = SZ(data);
+    vector<string> b;
+
+    if (!n) return nullptr;
+
+    string t;
+    for (auto x : data) {
+      if (x == ',') {
+        b.pb(t);
+        t = "";
+      } else
+        t += x;
+    }
+    b.pb(t);
+
+    for (auto x : b) {
+      if (x == "null")
+        a.pb(nullptr);
+      else
+        a.pb(new TN(stoi(x)));
+    }
+
+    int m = SZ(a);
+    int i = 0, j = 1;
+
+    while (i < m) {
+      while (i < m && !a[i]) ++i;
+      if (i >= n) break;
+
+      if (j < m) a[i]->left = a[j++];
+      if (j < m) a[i]->right = a[j++];
+      ++i;
+    }
+
+    return a[0];
+  }
+};
+
+LNP CreateList(VI &a) {
+  LN *du{new LN(0)}, *cur{}, *pre{du};
+
+  for (auto x : a) {
+    cur = new LN(x);
+    pre->next = cur;
+    pre = cur;
+  }
+
+  auto res = du->next;
+  delete du;
+  return res;
+}
+
+void PrintList(LNP head) {
+  while (head) {
+    dbgi(head->val);
+    head = head->next;
+  }
+  dbgln();
+}
+
+#endif
+// End of LeetCode
+
+class Solution {
+public:
+  double maxAmount(string initialCurrency,
+    vector<vector<string>>& pairs1, vector<double>& rates1,
+    vector<vector<string>>& pairs2, vector<double>& rates2) {
+
+    using PID = pair<int, double>;
+    using VD = vector<double>;
+    
+    map<string, int> m;
+    set<string> s;
+    s.insert(initialCurrency);
+    for (auto &e : pairs1) {
+      s.insert(e[0]);
+      s.insert(e[1]);
+    }
+    for (auto &e : pairs2) {
+      s.insert(e[0]);
+      s.insert(e[1]);
+    }
+    
+    int n = 0;
+    for (auto &x : s) m[x] = ++n;
+    
+    int start = m[initialCurrency];
+    
+    vector<vector<PID>> g1(n + 10), g2(n + 10);
+    VD dis1(n + 10);
+    VB vis1(n + 10);
+    int m1 = SZ(pairs1);
+    
+    For(i, 0, m1) {
+      VS &e = pairs1[i];
+      int u = m[e[0]], v = m[e[1]];
+      double w = rates1[i];
+      g1[u].pb({v, w});
+      g1[v].pb({u, 1 / w});
+    }
+    
+    dis1[start] = 1;
+    queue<int> q1;
+    q1.push(start);
+    
+    while (nemp(q1)) {
+      auto u = q1.front();
+      q1.pop();
+      if (vis1[u]) continue;
+      vis1[u] = true;
+      
+      double base = dis1[u];
+      for (auto [v, w] : g1[u]) {
+        dis1[v] = base * w;
+        if (!vis1[v])
+          q1.push(v);
+      }
+    }
+    
+    int m2 = SZ(pairs2);
+    
+    For(i, 0, m2) {
+      VS &e = pairs2[i];
+      double w = rates2[i];
+      int u = m[e[0]], v = m[e[1]];
+      g2[v].pb({u, w});
+      g2[u].pb({v, 1 / w});
+    }
+    
+    double res {1};
+    queue<int> q2;
+    VD dis2(n + 10);
+    VB vis2(n + 10);
+    
+    dis2[start] = 1;
+    q2.push(start);
+    
+    while (nemp(q2)) {
+      auto u = q2.front();
+      q2.pop();
+      
+      if (vis2[u]) continue;
+      vis2[u] = true;
+      
+      double base = dis2[u];
+      for (auto [v, w] : g2[u]) {
+        dis2[v]= base * w;
+        ckmax(res, dis2[v] * dis1[v]);
+        if (!vis2[v])
+          q2.push(v);
+      }
+    }
+
+    return res;
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  Solution a;
+
+  return 0;
+}
+
+#endif
+
+
