ba5fcc8b79b79e51a6af74ac7046f6362f9a01e6
liuxueyang <liuxueyang457@gmail.com>
2025-01-29 23:30:55 +0800

    leetcode contest 433 B 【组合数学，贡献法，乘法逆元】

---
+++ leetcode/contest/433/b.cc
@@ -0,0 +1,333 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T>
+bool ckmin(T &a, const T &b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T &a, const T &b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <typename t>
+istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec) in >> x;
+  return in;
+}
+
+template <typename t>
+ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1) out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode *
+#define TN TreeNode
+#define TNP TreeNode *
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+void PrePrintLCTree(TNP root) {
+  if (!root) return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+
+class LCCodec {
+ public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root) {
+    if (!root) return "";
+
+    vector<TNP> a;
+    a.pb(root);
+    string ans;
+
+    while (nemp(a)) {
+      vector<TNP> b;
+
+      string tmp;
+      for (auto x : a) {
+        if (nemp(ans)) {
+          ans += ',';
+        }
+
+        if (x)
+          ans += to_string(x->val);
+        else
+          ans += "null";
+      }
+
+      bool ok{false};
+      for (auto x : a) {
+        if (x) {
+          b.pb(x->left);
+          b.pb(x->right);
+
+          if (x->left || x->right) {
+            ok = true;
+          }
+        }
+      }
+
+      if (ok)
+        a = std::move(b);
+      else
+        a = {};
+    }
+
+    return ans;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data) {
+    vector<TNP> a;
+    int n = SZ(data);
+    vector<string> b;
+
+    if (!n) return nullptr;
+
+    string t;
+    for (auto x : data) {
+      if (x == ',') {
+        b.pb(t);
+        t = "";
+      } else
+        t += x;
+    }
+    b.pb(t);
+
+    for (auto x : b) {
+      if (x == "null")
+        a.pb(nullptr);
+      else
+        a.pb(new TN(stoi(x)));
+    }
+
+    int m = SZ(a);
+    int i = 0, j = 1;
+
+    while (i < m) {
+      while (i < m && !a[i]) ++i;
+      if (i >= n) break;
+
+      if (j < m) a[i]->left = a[j++];
+      if (j < m) a[i]->right = a[j++];
+      ++i;
+    }
+
+    return a[0];
+  }
+};
+
+LNP CreateList(VI &a) {
+  LN *du{new LN(0)}, *cur{}, *pre{du};
+
+  for (auto x : a) {
+    cur = new LN(x);
+    pre->next = cur;
+    pre = cur;
+  }
+
+  auto res = du->next;
+  delete du;
+  return res;
+}
+
+void PrintList(LNP head) {
+  while (head) {
+    dbgi(head->val);
+    head = head->next;
+  }
+  dbgln();
+}
+
+#endif
+// End of LeetCode
+
+class Solution {
+ public:
+  VL b, bv;
+
+  ll qpow(ll a, ll b, ll c) {
+    ll ans = 1 % c;
+
+    while (b) {
+      if (b & 1) ans = ans * a % c;
+      a = a * a % c;
+      b >>= 1;
+    }
+
+    return ans;
+  }
+
+  /// @brief 求数组 a 的每个元素在模 p 意义下的逆元
+  /// @param a 数组 a
+  /// @param p 质数 p
+  /// @param n 数组 a 中的下标为 [1, n] 的元素有意义
+  /// @return 数组 a 中 [1, n] 每个元素的逆元，答案数组中下标为 [1, n]
+  /// 的元素有意义
+  VL qinv(VL a, ll p, int n) {
+    VL av(n + 10, 1), s(av), sv(av);
+
+    For1(i, 1, n) { s[i] = s[i - 1] * a[i] % p; }
+    sv[n] = qpow(s[n], p - 2, p);
+
+    Rof(i, 1, n) sv[i] = sv[i + 1] * a[i + 1] % p;
+    Rof1(i, 1, n) av[i] = s[i - 1] * sv[i] % p;
+
+    return av;
+  }
+
+  ll C(int n, int m) {
+    if (m == 0 || m == n) return 1;
+    if (m > n) return 0;
+    return (b[n] * bv[m] % MOD) * bv[n - m] % MOD;
+  }
+
+  int minMaxSums(vector<int> &a1, int k) {
+    int n{SZ(a1)};
+    ll ans{};
+    sort(all(a1));
+
+    VI a(n + 10);
+    For(i, 0, n) a[i + 1] = a1[i];
+
+    b = VL(n + 10, 1);
+    For1(i, 1, n) { b[i] = b[i - 1] * i % MOD; }
+    bv = qinv(b, MOD, n);
+
+    For1(i, 1, n) {
+      ll cur{};
+      For1(j, 0, min(i - 1, k - 1)) { cur = (cur + C(i - 1, j)) % MOD; }
+      For1(j, 0, min(n - i, k - 1)) { cur = (cur + C(n - i, j)) % MOD; }
+      ans = (ans + cur * a[i] % MOD) % MOD;
+    }
+
+    return ans;
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  _m_gen64.seed(Pr);
+
+  Solution a;
+  VI ve;
+  int k, res;
+
+  ve = {1, 2, 3};
+  k = 2;
+  res = a.minMaxSums(ve, k);
+  dbg(res);
+
+  ve = {5, 0, 6};
+  k = 1;
+  res = a.minMaxSums(ve, k);
+  dbg(res);
+
+  return 0;
+}
+
+#endif
