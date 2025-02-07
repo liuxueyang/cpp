7f023e2688f8eb63f6de0f0c0efb593710f05f30
liuxueyang <liuxueyang457@gmail.com>
2025-02-07 23:03:16 +0800

    loj 145 DFS 序2 线段树做法【线段树区间更新】

--- LibreOJ/problemset/145_DFS_序_2_sol1.cpp
+++ LibreOJ/problemset/145_DFS_序_2_sol1.cpp
@@ -152,146 +152,99 @@ ostream &operator<<(ostream &os, const lll &v) {
 #define dbgr(x...)
 #endif
 
-struct Node {
-  int len;
-  ll sum, t;
-  Node() : len(1), sum(0), t(0) {}
+int n, m, r, ts;
+VI a, dfn, dfo;
+VVI g;
+VL c;
+
+struct BIT {
+  int n;
+  VL c;
+
+  BIT(int n_) : n(n_), c(n + 10) {}
+
+  int lowbit(int x) { return x & -x; }
+  ll query(int idx) {
+    ll ans{};
+    while (idx) {
+      ans += c[idx];
+      idx -= lowbit(idx);
+    }
+    return ans;
+  }
+  void modify(int idx, ll x) {
+    while (idx <= n) {
+      c[idx] += x;
+      idx += lowbit(idx);
+    }
+  }
 };
-
-const int N = 1000010;
-int a[N], dfn[N], dfo[N], b[N];
-int e[N * 2], ne[N * 2], h[N];
-int ts, idx;
-Node seg[N * 4];
-
-void Init() {
-  idx = ts = 0;
-  memset(h, -1, sizeof h);
-}
-
-void add(int a, int b) { e[idx] = b, ne[idx] = h[a], h[a] = idx++; }
 
 void dfs(int u, int fa) {
   dfn[u] = ++ts;
-  for (int i = h[u]; i != -1; i = ne[i]) {
-    int v = e[i];
-    if (v == fa || dfn[v]) continue;
+  for (auto v : g[u]) {
+    if (v == fa) continue;
     dfs(v, u);
   }
   dfo[u] = ts;
 }
 
-void update(int id) {
-  int left = id * 2, right = left + 1;
-  seg[id].sum = seg[left].sum + seg[right].sum;
-  seg[id].len = seg[left].len + seg[right].len;
-}
-
-void set_tag(int id, ll t) {
-  seg[id].t = seg[id].t + t;
-  seg[id].sum = seg[id].sum + t * seg[id].len;
-}
-
-void push_down(int id) {
-  int left = id * 2, right = left + 1;
-  auto &t = seg[id].t;
-  if (t != 0) {
-    set_tag(left, t), set_tag(right, t);
-    t = 0;
-  }
-}
-
-void build(int id, int l, int r) {
-  if (l == r) {
-    seg[id].sum = a[l];
-    return;
-  }
-
-  int mid = (l + r) / 2, left = id * 2, right = left + 1;
-  build(left, l, mid);
-  build(right, mid + 1, r);
-  update(id);
-}
-
-void modify(int id, int l, int r, int ql, int qr, ll t) {
-  if (ql == l && qr == r) {
-    set_tag(id, t);
-    return;
-  }
-
-  push_down(id);
-  int mid = (l + r) / 2, left = id * 2, right = left + 1;
-  if (qr <= mid)
-    modify(left, l, mid, ql, qr, t);
-  else if (ql > mid)
-    modify(right, mid + 1, r, ql, qr, t);
-  else {
-    modify(left, l, mid, ql, mid, t);
-    modify(right, mid + 1, r, mid + 1, qr, t);
-  }
-  update(id);
-}
-
-ll query(int id, int l, int r, int ql, int qr) {
-  if (l >= ql && r <= qr) {
-    return seg[id].sum;
-  }
-
-  push_down(id);
-  int mid = (l + r) / 2, left = id * 2, right = left + 1;
-  if (qr <= mid)
-    return query(left, l, mid, ql, qr);
-  else if (ql > mid)
-    return query(right, mid + 1, r, ql, qr);
-  else
-    return query(left, l, mid, ql, mid) + query(right, mid + 1, r, mid + 1, qr);
-}
-
 void solve() {
-  int n, m, r;
   cin >> n >> m >> r;
-  Init();
 
+  a = VI(n + 10);
   For1(i, 1, n) cin >> a[i];
-
+  g = VVI(n + 10);
   For(i, 0, n - 1) {
     int u, v;
     cin >> u >> v;
-    add(u, v), add(v, u);
+    g[u].pb(v);
+    g[v].pb(u);
   }
 
+  ts = 0;
+  dfn = dfo = VI(n + 10);
   dfs(r, -1);
-  For1(i, 1, n) {
-    int pos = dfn[i];
-    b[pos] = a[i];
-  }
-  For1(i, 1, n) a[i] = b[i];
 
-  build(1, 1, n);
+  BIT tree1(n), tree2(n);
+
+  For1(i, 1, n) {
+    int idx = dfn[i];
+
+    tree1.modify(idx, a[i]);
+    tree1.modify(idx + 1, -a[i]);
+
+    tree2.modify(idx, 1LL * idx * a[i]);
+    tree2.modify(idx + 1, 1LL * -(idx + 1) * a[i]);
+  }
 
   while (m--) {
-    int op, pos, x;
+    int op, u, x;
     cin >> op;
     if (op == 1) {
-      cin >> pos >> x;
-      int l = dfn[pos], r = dfo[pos];
-      modify(1, 1, n, l, r, x);
+      cin >> u >> x;
+      int l = dfn[u], r = dfo[u];
+
+      tree1.modify(l, x);
+      tree1.modify(r + 1, -x);
+
+      tree2.modify(l, 1LL * l * x);
+      tree2.modify(r + 1, 1LL * (r + 1) * -x);
     } else {
-      cin >> pos;
-      int l = dfn[pos], r = dfo[pos];
-      ll res = query(1, 1, n, l, r);
-      cout << res << '\n';
+      cin >> u;
+      int l = dfn[u], r = dfo[u];
+
+      cout << ((r + 1) * tree1.query(r) - l * tree1.query(l - 1)) -
+                  (tree2.query(r) - tree2.query(l - 1))
+           << '\n';
     }
   }
 }
 
 int main(void) {
 #ifdef _DEBUG
-#ifndef _CPH
-  freopen("../input.txt", "r", stdin);
+  // freopen("../input.txt", "r", stdin);
 #endif
-#endif
-
   std::ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
