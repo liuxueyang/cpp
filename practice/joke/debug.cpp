30b90201e8ea3d0894738575380684db34bfb587
liuxueyang <liuxueyang457@gmail.com>
2025-08-27 09:38:58 +0800

    Revert "Remove other OJs"
    This reverts commit d934da112b5e92e9333736b71c6dd6c3d5798f9e.

---
+++ practice/joke/debug.cpp
@@ -0,0 +1,184 @@
+// Date: Sun Jul 28 16:20:34 2024
+
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998244353;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+template <typename ForwardIterator>
+void Inputr(ForwardIterator begin, ForwardIterator end) {
+  ForwardIterator it = begin;
+  while (it != end) {
+    cin >> *it;
+    it++;
+  }
+}
+
+template <typename ForwardIterator>
+void Outputr(ForwardIterator begin, ForwardIterator end) {
+  ForwardIterator it = begin;
+  while (it != end) {
+    if (it != begin)
+      cout << ' ';
+    cout << *it;
+    it++;
+  }
+  NL;
+}
+
+template <typename T, typename ForwardIterator>
+void Outputr1(ForwardIterator begin, ForwardIterator end) {
+  copy(begin, end, ostream_iterator<T>(cout, " "));
+  NL;
+}
+
+const int N = 20;
+int a[N];
+
+void solve() {
+  int n = 10;
+
+  For1(i, 1, n) { a[i] = i * 2; }
+
+  dbgr(a + 1, a + 1 + n);
+
+  vector<PII> b(n);
+
+  For(i, 0, n) { b[i] = {i * 2, i * 2 + 1}; }
+
+  dbgr(b.begin(), b.end());
+
+  set<PII> s;
+
+  For(i, 0, n) { s.insert({i, i * 2}); }
+
+  dbgr(all(s));
+
+  cin >> n;
+
+  VI va(n);
+  int sum;
+
+  Inputr(a + 1, a + 1 + n);
+  Outputr(a + 1, a + 1 + n);
+  sum = accumulate(a + 1, a + 1 + n, 0);
+  dbg(sum);
+  Outputr(all1(a, n));
+
+  // Inputr(all(va));
+  // Outputr(all(va));
+
+  // sum = reduce(all(va));
+  // dbg(sum);
+
+  sum = 0;
+  for_each(all(va), [&](int x) { sum += x; });
+  dbg(sum);
+
+  sum = accumulate(all(va), 0);
+  dbg(sum);
+
+  int mx = *max_element(all(va), [&](int x, int y) { return x > y; });
+  dbg(mx);
+
+  copy(all(va), ostream_iterator<int>(cout, " "));
+  NL;
+  Outputr1<int>(all(va));
+}
+
+int main(void) {
+#ifdef _DEBUG
+  freopen("debug.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int T = 1;
+
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
---
+++ practice/spfa/4779.cpp
@@ -0,0 +1,251 @@
+// Date: Sat Dec 30 18:12:48 2023
+
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <algorithm>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T> bool ckmin(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+template <typename t> istream &operator>>(istream &in, vector<t> &vec) {
+  for (t &x : vec)
+    in >> x;
+  return in;
+}
+
+template <typename t> ostream &operator<<(ostream &out, vector<t> &vec) {
+  int n = SZ(vec);
+  For(i, 0, n) {
+    out << vec[i];
+    if (i < n - 1)
+      out << ' ';
+  }
+  return out;
+}
+
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void __print(const pair<T, V> &x) {
+  cerr << '{';
+  __print(x.first);
+  cerr << ", ";
+  __print(x.second);
+  cerr << '}';
+}
+template <typename T> void __print(const T &x) {
+  int f = 0;
+  cerr << '{';
+  for (auto &i : x)
+    cerr << (f++ ? ", " : ""), __print(i);
+  cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v) {
+  __print(t);
+  if (sizeof...(v))
+    cerr << ", ";
+  _print(v...);
+}
+
+#ifdef _DEBUG
+#define debug1(x) cout << #x " = " << x << endl;
+#define debug2(x, y) cout << #x " = " << x << " " #y " = " << y << endl;
+#define debug3(x, y, z)                                                        \
+  cout << #x " = " << x << " " #y " = " << y << " " #z " = " << z << endl;
+#define dbg(x...)                                                              \
+  cerr << "\e[91m" << __func__ << ":" << __LINE__ << " [" << #x << "] = [";    \
+  _print(x);                                                                   \
+  cerr << "\e[39m" << endl;
+#else
+#define debug1
+#define debug2
+#define debug3
+#define dbg(x...)
+#endif
+
+const int N = 100100, M = 500100;
+
+int s, n, m, idx, dis[N], h[N], st[N];
+int e[M], ne[M], w[M];
+
+void Init() {
+  idx = 0;
+  memset(h, -1, sizeof h);
+  memset(st, 0, sizeof st);
+  memset(dis, 0x3f, sizeof dis);
+}
+
+void Add(int a, int b, int c) {
+  e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+
+// void spfa(int s) {
+//   dis[s] = 0;
+
+//   queue<int> q;
+//   q.push(s);
+//   st[s] = 1;
+
+//   while (nemp(q)) {
+//     int t = q.front();
+//     q.pop();
+
+//     st[t] = 0;
+
+//     ForE(i, t) {
+//       int j = e[i], tmp = dis[t] + w[i];
+//       if (tmp < dis[j]) {
+//         dis[j] = tmp;
+
+//         if (!st[j]) {
+//           st[j] = 1;
+//           q.push(j);
+//         }
+//       }
+
+//       // if (ckmin(dis[j], dis[t] + w[i]) && !st[j]) {
+//       //   q.push(j);
+//       //   st[j] = 1;
+//       // }
+//     }
+//   }
+
+//   ll top = (1LL << 31) - 1;
+//   For1(i, 1, n) {
+//     printf("%lld ", dis[i] == INF ? top : ll(dis[i]));
+//     // cout << (dis[i] == INF ? top : dis[i]) << ' ';
+//   }
+//   printf("\n");
+//   // NL;
+// }
+//
+
+void spfa(int s) {
+  dis[s] = 0;
+  st[s] = 1;
+
+  queue<int> q({s});
+
+  while (!q.empty()) {
+    int t = q.front();
+    q.pop();
+
+    st[t] = 0;
+
+    for (int i = h[t]; i != -1; i = ne[i]) {
+      int j = e[i], tmp = dis[t] + w[i];
+      if (tmp < dis[j]) {
+        dis[j] = tmp;
+        if (!st[j]) {
+          st[j] = 1;
+          q.push(j);
+        }
+      }
+    }
+  }
+
+  for (int i = 1; i <= n; ++i)
+    printf("%lld ", dis[i] == INF ? ((1LL << 31) - 1) : dis[i]);
+  puts("");
+}
+
+void solve() {
+  // Init();
+  // scanf("%d%d%d", &n, &m, &s);
+  // // cin >> n >> m >> s;
+
+  // For(i, 0, m) {
+  //   int a, b, c;
+  //   scanf("%d%d%d", &a, &b, &c);
+  //   // cin >> a >> b >> c;
+  //   Add(a, b, c);
+  // }
+
+  scanf("%d%d%d", &n, &m, &s);
+  Init();
+
+  while (m--) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    Add(a, b, c);
+  }
+
+  spfa(s);
+}
+
+int main(void) {
+#ifdef _DEBUG
+  freopen("4779.in", "r", stdin);
+#endif
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int T = 1;
+
+  while (T--) {
+    solve();
+  }
+
+  return 0;
+}
