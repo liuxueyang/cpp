46ccb8e408931039c4fce315349fcc25e0fab8ba
liuxueyang <liuxueyang457@gmail.com>
2025-09-16 22:27:40 +0800

    更新 CleanVS.bat

--- CleanVS.bat
+++ CleanVS.bat
@@ -1,10 +1,16 @@
 @echo off
 echo Closing Visual Studio...
 taskkill /f /im devenv.exe >nul 2>&1
-echo Cleaning up .vs directories...
-for /d /r . %%d in (.vs) do if exist "%%d" (
-    echo Deleting: %%d
-    rd /s /q "%%d"
+
+set DIRS_TO_CLEAN=.vs x64
+
+for %%n in (%DIRS_TO_CLEAN%) do (
+    echo Cleaning up %%n directories...
+    for /d /r . %%d in (%%n) do if exist "%%d" (
+        echo Deleting: %%d
+        rd /s /q "%%d"
+    )
 )
+
 echo Cleanup complete!
 pause
