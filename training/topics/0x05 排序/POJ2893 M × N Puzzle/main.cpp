301a90db6d1c1cc54b0e34c0226e4321314723b9
liuxueyang <liuxueyang457@gmail.com>
2025-09-25 22:03:54 +0800

    POJ2893 M × N Puzzle

---
+++ training/topics/0x05 排序/POJ2893 M × N Puzzle/main.cpp
@@ -0,0 +1,269 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string> > VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool> > VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+template <class T>
+struct pq {
+  typedef priority_queue<T> type;
+};
+template <class T>
+struct pqg {
+  typedef priority_queue<T, vector<T>, greater<T> > type;
+};
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998244353;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+#define No() cout << "No\n"
+#define Yes() cout << "Yes\n"
+#define NO() cout << "NO\n"
+#define YES() cout << "YES\n"
+
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    os << a[i] << " \n"[i == n];
+  }
+  return os;
+}
+
+template <class T>
+ostream& operator<<=(ostream& os, const vector<T>& a) {
+  int n = int(a.size());
+  for (int i = 0; i < n; ++i) {
+    os << a[i] << " \n"[i == n - 1];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>=(istream& is, vector<T>& a) {
+  int len = int(a.size());
+  for (int i = 0; i < len; i++) {
+    is >> a[i];
+  }
+  return is;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    is >> a[i];
+  }
+  return is;
+}
+
+template <typename ForwardIterator>
+void Inputr(ForwardIterator begin, ForwardIterator end) {
+  ForwardIterator it = begin;
+  while (it != end) {
+    cin >> *it;
+    it++;
+  }
+}
+
+template <typename ForwardIterator>
+void Outputr(ForwardIterator begin, ForwardIterator end) {
+  ForwardIterator it = begin;
+  while (it != end) {
+    if (it != begin) cout << ' ';
+    cout << *it;
+    it++;
+  }
+  NL;
+}
+
+template <typename T, typename ForwardIterator>
+void Outputr1(ForwardIterator begin, ForwardIterator end) {
+  copy(begin, end, ostream_iterator<T>(cout, " "));
+  NL;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+void Init();
+void solve();
+
+int main(void) {
+#if defined(_DEBUG) && !defined(_CPH)
+  freopen("input.txt", "r", stdin);
+#endif
+
+#ifdef _DEBUG
+  clock_t _start_ts = clock();
+#endif
+
+  Init();
+  solve();
+
+#ifdef _DEBUG
+  clock_t _end_ts = clock();
+  std::cerr << "Execution time: "
+            << double(_end_ts - _start_ts) / CLOCKS_PER_SEC << " seconds."
+            << std::endl;
+#endif
+
+  return 0;
+}
+
+void Init() {}
+
+const int N = 1e6 + 10;
+int a[N], b[N];
+
+struct MergeSort {
+  ll ans;
+
+  MergeSort(int n) : ans(0LL) { merge(0, n - 1); }
+
+  void merge(int l, int r) {
+    if (l >= r) return;
+
+    int mid = (l + r) >> 1;
+    merge(l, mid);
+    merge(mid + 1, r);
+
+    int i = l, j = mid + 1;
+    For1(k, l, r) {
+      if (j > r || i <= mid && a[j] >= a[i])
+        b[k] = a[i++];
+      else {
+        b[k] = a[j++];
+        ans += mid - i + 1;
+      }
+    }
+
+    For1(k, l, r) a[k] = b[k];
+  }
+};
+
+void read(int& x) {
+  bool neg = false;
+  x = 0;
+  char ch = 0;
+  while (ch < '0' || ch > '9') {
+    if (ch == '-') neg = true;
+    ch = getchar();
+  }
+  if (neg) {
+    while (ch >= '0' && ch <= '9') {
+      x = x * 10 + ('0' - ch);
+      ch = getchar();
+    }
+  } else {
+    while (ch >= '0' && ch <= '9') {
+      x = x * 10 + (ch - '0');
+      ch = getchar();
+    }
+  }
+}
+
+void solve() {
+  int n, m;
+  while (1) {
+    read(n);
+    read(m);
+    if (!n || !m) break;
+    int row1 = -1, row2 = n - 1, idx = 0;
+
+    For(i, 0, n) {
+      For(j, 0, m) {
+        int x;
+        read(x);
+        if (x) {
+          a[idx++] = x;
+        } else {
+          row1 = i;
+        }
+      }
+    }
+
+    MergeSort msa(n * m - 1);
+
+    if (m & 1) {
+      if (msa.ans % 2 == 0)
+        printf("YES\n");
+      else
+        printf("NO\n");
+    } else {
+      int cnt1 = msa.ans & 1, cnt2 = abs(row2 - row1);
+      if ((cnt1 + cnt2) % 2 == 0)
+        printf("YES\n");
+      else
+        printf("NO\n");
+    }
+  }
+}
