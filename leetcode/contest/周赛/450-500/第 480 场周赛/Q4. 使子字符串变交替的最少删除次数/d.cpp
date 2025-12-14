7ebb266f0dd8f026ef53407db2e94b57ecd82beb
liuxueyang <liuxueyang457@gmail.com>
2025-12-14 15:51:42 +0800

    第 480 场周赛 ABCD

---
+++ leetcode/contest/周赛/450-500/第 480 场周赛/Q4. 使子字符串变交替的最少删除次数/d.cpp
@@ -0,0 +1,399 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    os << a[i] << " \n"[i == n];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    is >> a[i];
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<=(ostream& os, const vector<T>& a) {
+  int n = int(a.size());
+  for (int i = 0; i < n; ++i) {
+    os << a[i] << " \n"[i == n - 1];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>=(istream& is, vector<T>& a) {
+  for (auto& x : a) is >> x;
+  return is;
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#define dbgi(x)
+#define dbgln()
+#define dbgr(x...)
+#endif
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+
+#ifdef _DEBUG
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode* next) : val(val), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+void PrePrintLCTree(TNP root) {
+  if (!root) return;
+  dbgi(root->val);
+  PrePrintLCTree(root->left);
+  PrePrintLCTree(root->right);
+}
+
+class LCCodec {
+ public:
+  // Encodes a tree to a single string.
+  string serialize(TreeNode* root) {
+    if (!root) return "";
+
+    vector<TNP> a;
+    a.pb(root);
+    string ans;
+
+    while (nemp(a)) {
+      vector<TNP> b;
+
+      string tmp;
+      for (auto x : a) {
+        if (nemp(ans)) {
+          ans += ',';
+        }
+
+        if (x)
+          ans += to_string(x->val);
+        else
+          ans += "null";
+      }
+
+      bool ok{false};
+      for (auto x : a) {
+        if (x) {
+          b.pb(x->left);
+          b.pb(x->right);
+
+          if (x->left || x->right) {
+            ok = true;
+          }
+        }
+      }
+
+      if (ok)
+        a = std::move(b);
+      else
+        a = {};
+    }
+
+    return ans;
+  }
+
+  // Decodes your encoded data to tree.
+  TreeNode* deserialize(string data) {
+    vector<TNP> a;
+    int n = SZ(data);
+    vector<string> b;
+
+    if (!n) return nullptr;
+
+    string t;
+    for (auto x : data) {
+      if (x == ',') {
+        b.pb(t);
+        t = "";
+      } else
+        t += x;
+    }
+    b.pb(t);
+
+    for (auto x : b) {
+      if (x == "null")
+        a.pb(nullptr);
+      else
+        a.pb(new TN(stoi(x)));
+    }
+
+    int m = SZ(a);
+    int i = 0, j = 1;
+
+    while (i < m) {
+      while (i < m && !a[i]) ++i;
+      if (i >= n) break;
+
+      if (j < m) a[i]->left = a[j++];
+      if (j < m) a[i]->right = a[j++];
+      ++i;
+    }
+
+    return a[0];
+  }
+};
+
+LNP CreateList(VI a) {
+  LN *du{new LN(0)}, *cur{}, *pre{du};
+
+  for (auto x : a) {
+    cur = new LN(x);
+    pre->next = cur;
+    pre = cur;
+  }
+
+  auto res = du->next;
+  delete du;
+  return res;
+}
+
+void PrintList(LNP head) {
+  while (head) {
+    dbgi(head->val);
+    head = head->next;
+  }
+  dbgln();
+}
+
+#endif
+// End of LeetCode
+
+const int N = 100100;
+
+struct Info {
+  int mss, pre_len, suf_len, len;
+  char pre_ch, suf_ch;
+
+  Info operator+(const Info& rh) const {
+    Info res;
+
+    res.len = len + rh.len;
+
+    res.pre_ch = pre_ch;
+    res.pre_len = pre_len;
+    if (pre_len == len && pre_ch == rh.pre_ch) {
+      res.pre_len = len + rh.pre_len;
+    }
+
+    res.suf_ch = rh.suf_ch;
+    res.suf_len = rh.suf_len;
+    if (rh.suf_len == rh.len && rh.suf_ch == suf_ch) {
+      res.suf_len = rh.len + suf_len;
+    }
+    if (suf_ch == rh.pre_ch) {
+      res.mss = (mss - (suf_len - 1)) + (rh.mss - (rh.pre_len - 1)) +
+                (suf_len + rh.pre_len) - 1;
+    } else {
+      res.mss = mss + rh.mss;
+    }
+
+    return res;
+  }
+};
+
+struct Node {
+  Info info;
+};
+
+int n, q, b[N];
+Node a[N * 4];
+string s;
+
+void Init() {}
+
+PII children(int id) { return {id * 2, id * 2 + 1}; }
+
+void update(int id) {
+  auto [lc, rc] = children(id);
+  a[id].info = a[lc].info + a[rc].info;
+}
+
+void build(int id, int l, int r) {
+  if (l == r) {
+    a[id] = {0, 1, 1, 1, s[l], s[l]};
+    return;
+  }
+  int mid = (l + r) / 2;
+  auto [lc, rc] = children(id);
+  build(lc, l, mid);
+  build(rc, mid + 1, r);
+  update(id);
+}
+
+void change(int id, int l, int r, int pos, char val) {
+  if (l == r) {
+    a[id] = {0, 1, 1, 1, val, val};
+    return;
+  }
+  int mid = (l + r) / 2;
+  auto [lc, rc] = children(id);
+  if (pos <= mid)
+    change(lc, l, mid, pos, val);
+  else
+    change(rc, mid + 1, r, pos, val);
+  update(id);
+}
+
+Info query(int id, int l, int r, int ql, int qr) {
+  if (l >= ql && r <= qr) {
+    return a[id].info;
+  }
+  int mid = (l + r) / 2;
+  auto [lc, rc] = children(id);
+  if (qr <= mid)
+    return query(lc, l, mid, ql, qr);
+  else if (ql > mid)
+    return query(rc, mid + 1, r, ql, qr);
+  else
+    return query(lc, l, mid, ql, mid) + query(rc, mid + 1, r, mid + 1, qr);
+}
+
+class Solution {
+ public:
+  vector<int> minDeletions(string s0, vector<vector<int>>& queries) {
+    n = SZ(s0);
+    s = " " + s0;
+    build(1, 1, n);
+
+    VI ans;
+    for (auto& ve : queries) {
+      int op = ve[0];
+      if (op == 1) {
+        int idx = ve[1];
+        idx++;
+        char tar = s[idx];
+        if (tar == 'A')
+          tar = 'B';
+        else
+          tar = 'A';
+        s[idx] = tar;
+
+        change(1, 1, n, idx, tar);
+      } else {
+        int l = ve[1], r = ve[2];
+        l++, r++;
+        auto res = query(1, 1, n, l, r);
+        ans.pb(res.mss);
+      }
+    }
+    return ans;
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  // freopen("input.txt", "r", stdin);
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  Solution a;
+
+  return 0;
+}
+
+#endif
