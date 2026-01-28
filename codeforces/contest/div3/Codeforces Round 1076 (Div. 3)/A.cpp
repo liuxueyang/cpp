a747aa4d578cec9105da8889fcf12695638d8fe6
liuxueyang <liuxueyang457@gmail.com>
2026-01-28 16:06:23 +0800

    Codeforces Round 1076 (Div. 3) ABCDEF

---
+++ codeforces/contest/div3/Codeforces Round 1076 (Div. 3)/A.cpp
@@ -0,0 +1,186 @@
+// Problem: A. DBMB and the Array
+// Contest: Codeforces - Codeforces Round 1076 (Div. 3)
+// URL: https://codeforces.com/contest/2193/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998244353;
+const ll INFL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+#define No() cout << "No\n"
+#define Yes() cout << "Yes\n"
+#define NO() cout << "NO\n"
+#define YES() cout << "YES\n"
+
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <class T>
+ostream& operator<<=(ostream& os, const vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    os << a[i] << " \n"[i == n];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>=(istream& is, vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    is >> a[i];
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& a) {
+  int n = int(a.size());
+  for (int i = 0; i < n; ++i) {
+    os << a[i] << " \n"[i == n - 1];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& a) {
+  for (auto& x : a) is >> x;
+  return is;
+}
+
+void read(int& x) {
+  bool neg = false;
+  x = 0;
+  char ch = 0;
+  while (ch < '0' || ch > '9') {
+    if (ch == '-') neg = true;
+    ch = getchar();
+  }
+  if (neg) {
+    while (ch >= '0' && ch <= '9') {
+      x = x * 10 + ('0' - ch);
+      ch = getchar();
+    }
+  } else {
+    while (ch >= '0' && ch <= '9') {
+      x = x * 10 + (ch - '0');
+      ch = getchar();
+    }
+  }
+}
+
+void write(int x) {
+  bool neg = false;
+  if (x < 0) {
+    neg = true;
+    putchar('-');
+  }
+  static int sta[40];
+  int top = 0;
+  do {
+    sta[top++] = x % 10;
+    x /= 10;
+  } while (x);
+  if (neg)
+    while (top) putchar('0' - sta[--top]);
+  else
+    while (top) putchar('0' + sta[--top]);
+}
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+
+void Init();
+void solve();
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int T = 1;
+  cin >> T;
+  while (T--) {
+    Init();
+    solve();
+  }
+
+  return 0;
+}
+
+void Init() {}
+
+void solve() {
+  int n, s, x;
+  cin >> n >> s >> x;
+
+  VI a(n);
+  cin >> a;
+
+  int sum = 0;
+  for (auto x : a) sum += x;
+
+  if (s < sum)
+    NO();
+  else {
+    int g = s - sum;
+    if (g % x == 0)
+      YES();
+    else
+      NO();
+  }
+}
