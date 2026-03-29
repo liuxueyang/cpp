cb3384e4756ec73ab306f2e486f46ed858cffaa5
liuxueyang <liuxueyang457@gmail.com>
2026-03-29 10:19:41 +0800

    LeetCode contest 494

---
+++ leetcode/contest/周赛/450-500/第 494 场周赛/Q2. 构造奇偶一致的数组 II/b.cpp
@@ -0,0 +1,192 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <class T>
+ostream& operator<<=(ostream& os, const vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    os << a[i] << " \n"[i == n];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>=(istream& is, vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    is >> a[i];
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& a) {
+  int n = int(a.size());
+  for (int i = 0; i < n; ++i) {
+    os << a[i] << " \n"[i == n - 1];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& a) {
+  for (auto& x : a) is >> x;
+  return is;
+}
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+// End of LeetCode
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+
+class Solution {
+ public:
+  bool uniformArray(vector<int>& a) {
+    int n{SZ(a)}, cnt0{}, cnt1{};
+    for (auto x : a) {
+      if (x & 1)
+        cnt1++;
+      else
+        cnt0++;
+    }
+    if (cnt0 == n || cnt1 == 0) return true;
+
+    sort(all(a));
+    // odd
+    int mx_odd = INF;
+    bool ok = true;
+    for (auto x : a) {
+      if (x & 1) {
+        ckmin(mx_odd, x);
+      } else {
+        if (mx_odd < x) {
+        } else {
+          ok = false;
+          break;
+        }
+      }
+    }
+    if (ok) return true;
+
+    // even
+    ok = true;
+    mx_odd = INF;
+    for (auto x : a) {
+      if (x % 2 == 0)
+        continue;
+      else {
+        if (mx_odd < x)
+          ;
+        else {
+          ok = false;
+          break;
+        }
+        ckmax(mx_odd, x);
+      }
+    }
+    if (ok) return true;
+    return false;
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  Solution a;
+  int n;
+  cin >> n;
+  VI arr(n);
+  cin >> arr;
+  auto res = a.uniformArray(arr);
+  cout << res << '\n';
+
+  return 0;
+}
+
+#endif
