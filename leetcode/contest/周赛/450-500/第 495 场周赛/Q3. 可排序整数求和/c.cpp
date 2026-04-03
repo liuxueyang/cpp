c1fac2db7f90736342749239c47e1bae5198f45f
liuxueyang <liuxueyang457@gmail.com>
2026-04-03 17:59:12 +0800

    leetcode contest

---
+++ leetcode/contest/周赛/450-500/第 495 场周赛/Q3. 可排序整数求和/c.cpp
@@ -0,0 +1,208 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef vector<vector<string>> VVS;
+typedef vector<bool> VB;
+typedef vector<vector<bool>> VVB;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9 + 7, MOD1 = 998'244'353;
+const ll INFL = 0x3f3f3f3f'3f3f3f3f;
+const double eps = 1e-8;
+const int dir[8][2] = {
+    {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+};
+mt19937_64 _m_gen64;
+
+const ull Pr = 131, Pr1 = 13331;
+
+#define For(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define Rof(i, a, b) for (int i = int(b) - 1; i >= int(a); --i)
+#define For1(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define Rof1(i, a, b) for (int i = int(b); i >= int(a); --i)
+#define ForE(i, j) for (int i = h[j]; i != -1; i = ne[i])
+
+#define f1 first
+#define f2 second
+#define pb push_back
+#define has(a, x) (a.find(x) != a.end())
+#define nemp(a) (!a.empty())
+#define all(a) (a).begin(), (a).end()
+#define all1(a, len) (a + 1), (a + 1 + len)
+#define SZ(a) int((a).size())
+#define NL cout << '\n';
+
+template <class T>
+bool ckmin(T& a, const T& b) {
+  return b < a ? a = b, 1 : 0;
+}
+template <class T>
+bool ckmax(T& a, const T& b) {
+  return a < b ? a = b, 1 : 0;
+}
+
+template <class T>
+ostream& operator<<=(ostream& os, const vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    os << a[i] << " \n"[i == n];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>=(istream& is, vector<T>& a) {
+  int n = int(a.size()) - 1;
+  for (int i = 1; i <= n; ++i) {
+    is >> a[i];
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& a) {
+  int n = int(a.size());
+  for (int i = 0; i < n; ++i) {
+    os << a[i] << " \n"[i == n - 1];
+  }
+  return os;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& a) {
+  for (auto& x : a) is >> x;
+  return is;
+}
+
+// For LeetCode
+#define LN ListNode
+#define LNP ListNode*
+#define TN TreeNode
+#define TNP TreeNode*
+// End of LeetCode
+
+#ifdef _DEBUG
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+
+class Solution {
+ public:
+  int sortableIntegers(vector<int>& a) {
+    int n{SZ(a)}, ans{};
+    VI b;
+    for (int i = 1; i * i <= n; i++) {
+      if (n % i == 0) {
+        int j = n / i;
+        if (i == j)
+          b.pb(i);
+        else {
+          b.pb(i);
+          b.pb(j);
+        }
+      }
+    }
+    sort(all(b));
+    b.resize(distance(b.begin(), unique(all(b))));
+
+    auto check = [&](VI& vec) -> bool {
+      int len = SZ(vec);
+      For(i, 1, len) {
+        if (vec[i] < vec[i - 1]) return false;
+      }
+      return true;
+    };
+
+    auto shift_left = [&](VI& vec, int k) {
+      int n = SZ(vec);
+      if (k == 0 || k == n) return;
+      reverse(vec.begin(), vec.begin() + k);
+      reverse(vec.begin() + k, vec.end());
+      reverse(vec.begin(), vec.end());
+    };
+
+    for (auto k : b) {
+      bool ok = true;
+      int pre = -INF;
+
+      for (int i = 0; i + k <= n; i += k) {
+        int j = i + k;
+        VI a1(a.begin() + i, a.begin() + j);
+        int me{INF}, pos{-1}, len{SZ(a1)}, mx{-INF};
+
+        For(i1, 0, len) {
+          if (ckmin(me, a1[i1])) {
+            pos = i1;
+          }
+          ckmax(mx, a1[i1]);
+        }
+        shift_left(a1, pos);
+
+        if (!(check(a1) && a1[0] >= pre)) {
+          ok = false;
+          break;
+        }
+        pre = a1.back();
+      }
+      if (ok) {
+        ans += k;
+      }
+    }
+    return ans;
+  }
+};
+
+#ifdef _DEBUG
+
+int main(void) {
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  Solution a;
+  int n;
+  cin >> n;
+  VI arr(n);
+  cin >> arr;
+  auto res = a.sortableIntegers(arr);
+  cout << res << '\n';
+
+  return 0;
+}
+
+#endif
